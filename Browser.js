d7a37d1b800023dd026861abe7909f474c86baae
Nouman Abbasi <thenoumanabbasi@gmail.com>
2019-11-14 16:08:35 +0500

    Added separate func for adding pieces

--- Browser.js
+++ Browser.js
@@ -21,10 +21,12 @@ new Vue({                   // Grid indexing starts from bottom left cornor (But
         ROW_NUM: 6,
         COL_NUM: 7,
         colChoice: -1,
-        myChoicePos: [],       // stores the position at where the new piece is placed
+        myChoicePos: [],        // stores the position at where the new piece is placed
         oppChoicePos: [],
-        myPieceColor: 'X',      // color of piece assigned to this user by the server. Player 1: X. Player 2: O.
+        myPiece: '',      // color of piece assigned to this user by the server. Player 1: X. Player 2: O.
+        oppPiece: '',
         board: [],
+        boardTest: [],          // this board is used for trying out next opponent moves for highlighting columns 
         infoMsg: '',
         gameStarted: false,
         gameEnded: false,
@@ -32,14 +34,24 @@ new Vue({                   // Grid indexing starts from bottom left cornor (But
         ws: new WebSocket('ws://localhost:5000')
     },
     methods: {
-        async sendChoice() {
-            this.ws.send(JSON.stringify(this.myChoicePos))
+        async sendChoice(col) {
+            this.ws.send(JSON.stringify(col))
         },
         
-        updateBoard (row, col, piece) {     // the reactive version of doing board[i][col] = pieceColor 
-            let newRow = this.board[row].slice(0)
+        updateBoard(board, row, col, piece) {   // the reactive version of doing board[i][col] = pieceColor
+            let newRow = board[row].slice(0)
             newRow[col] = piece
-            this.$set(this.board, row, newRow)
+            this.$set(board, row, newRow)
+        },
+
+        addPieceOnBoard(board, col, piece) {
+            // Finding the next empty space in column to fit new piece in 
+            for (let i = 0; i < this.ROW_NUM; i++) {
+                if (this.board[i][col] === ' ') {
+                    this.updateBoard(board, i, col, piece)
+                    return [i, col]
+                }
+            }
         },
 
         makeMove(event) {
@@ -52,26 +64,24 @@ new Vue({                   // Grid indexing starts from bottom left cornor (But
                 return
             }
 
-            this.colChoice = event.target.cellIndex
+            // this.colChoice = event.target.cellIndex
             let col = event.target.cellIndex
-            console.log(col)
+            console.log('My choice of col: ', col)
             if (this.board[this.ROW_NUM-1][col] !== ' ') {   // If no more space left in that column
                 this.infoMsg = 'No more space left. Choose another column.'
             } else {
-                // Finding the next empty space to add the piece
-                for (let i = 0; i < this.ROW_NUM; i++) {
-                    if (this.board[i][col] === ' ') {
-                        this.updateBoard(i, col, this.myPieceColor)
-                        this.myChoicePos = [i, col]
-                        break
-                    }
-                }
-                this.colChoice = -1
+                this.myChoicePos = this.addPieceOnBoard(this.board, col, this.myPiece)
                 this.myTurn = false
                 this.sendChoice(this.myChoicePos)
             }
         },
 
+    },
+
+    testMove(event) {
+        let col = event.target.cellIndex
+        let boardTest = JSON.parse(JSON.stringify(this.board))     // taking updated copy of board
+        addPieceOnBoard(boardTest, col, this.myPiece)
     },
 
     created() {
@@ -83,12 +93,14 @@ new Vue({                   // Grid indexing starts from bottom left cornor (But
             if (!this.gameStarted) {
                 if (event.data === '1') {         // If this is player 1
                     console.log('Connected with Server.\nYou are player 1. Waiting for player 2...')
-                    this.myPieceColor = 'X'     // player 1 assigned color 'X'
+                    this.myPiece = 'X'     // player 1 assigned color 'X'
+                    this.oppPiece = 'O'
                     this.myTurn = true
                 }
                 else if (event.data === '2') {         // If this is player 2
                     console.log('Connected with Server.\nYou are player 2.')
-                    this.myPieceColor = 'O'     // player 2 assigned color 'O'
+                    this.myPiece = 'O'     // player 2 assigned color 'O'
+                    this.oppPiece = 'X'
                     this.myTurn = false
                 }
                 else if (event.data === '3') {    // 3 is indication from server to start game
@@ -108,11 +120,8 @@ new Vue({                   // Grid indexing starts from bottom left cornor (But
                 } else {
                     this.infoMsg = ''
                     let [oppRow, oppCol] = JSON.parse(event.data)
-                    console.log('Opponent choice received', oppRow, oppCol)
-                    if (this.myPieceColor === 'X')
-                        this.updateBoard(oppRow, oppCol, 'O')
-                    else
-                        this.updateBoard(oppRow, oppCol, 'X')
+                    console.log('Opponent choice of col: ', oppCol)
+                    this.addPieceOnBoard(this.board, oppCol, this.oppPiece)
                     this.myTurn = true
                 }
             }
