d476321141063d9bf33e46b10f1010dfbff2ee3b
kiciorleszek@gmail.com <kebab0>
2022-04-28 18:56:13 +0200

    Server now supports an arbitrary number of concurrent games based on token used to connect

--- Server.js
+++ Server.js
@@ -2,46 +2,58 @@ const http = require('http')
 const ws = require('ws')
 
 const PORT = 5000
+const SERVER_MESSAGES = {
+    YOU_ARE_PLAYER_ONE: '1',
+    YOU_ARE_PLAYER_TWO: '2',
+    START_GAME: '3'
+}
+const playerGroupMap = {}
 
 const server = http.createServer().listen(PORT)
+const wsServer = new ws.WebSocketServer({server});
 
-let first, second
 
-const playGame = (player1, player2) => {
-    player1.on('message', msg => {
-        console.log('Msg received from player1: ', msg)
-        player2.send(msg)
+const setupGameEvents = (playerGroup) => {
+    playerGroup.first.on('message', msg => {
+        console.log('Message received from player 1: ', msg)
+        playerGroup.second.send(msg)
     })
-
-    player2.on('message', msg => {
-        console.log('Msg received from player2: ', msg)
-        player1.send(msg)
+    playerGroup.second.on('message', msg => {
+        console.log('Message received from player 2: ', msg)
+        playerGroup.first.send(msg)
     })
 }
 
-new ws.Server({ server }).on('connection', client => {
-    if (first) {        // If both players have been connected
-        second = client
-        console.log('Player 2 connected as well. Initiating game.')
-        second.send('2')      // signalling player that you are player 2
+const startGame = playerGroup => {
+    playerGroup.first.send(SERVER_MESSAGES.START_GAME)
+    playerGroup.second.send(SERVER_MESSAGES.START_GAME)
+}
 
-        // Now telling both players to start game
-        first.send('3')
-        second.send('3')
 
-        playGame(first, second)
-        
-        first = undefined
-        second = undefined
-    } else {
-        // Only one player connected so far
-        first = client
-        console.log('Player 1 connected. Waiting for Player 2...')
-        client.send('1')      // signalling player that you are player 1
+wsServer.on('connection', (client, request) => {
+    const token = request.url.split('=')[1]
+    const playerGroup = playerGroupMap[token]
+    if (!playerGroup) { // No player group  -> create new
+        playerGroupMap[token] = {first: client, second: null}
+        console.log(`New player group created with token ${token}`)
+        client.send(SERVER_MESSAGES.YOU_ARE_PLAYER_ONE) // Tell client it's player 1
+    } else { // Player group already exists -> join
+        playerGroup.second = client
+        console.log(`New player added to group ${token}`)
+        client.send(SERVER_MESSAGES.YOU_ARE_PLAYER_TWO) // Tell client it's player 2
+        setupGameEvents(playerGroup)
+        startGame(playerGroup)
+        delete playerGroupMap[token]
+        console.log('GAME STARTED')
+        if(Object.keys(playerGroupMap).length) {
+            console.log(`Groups waiting to start: ${Object.keys(playerGroupMap)}`)
+        } else {
+            console.log('No remaining waiting groups left.')
+        }
     }
 
     client.on('close', () => {
-        console.log('A player disconnected')
+        console.log(`A player disconnected from group ${token}`)
     })
 })
 
